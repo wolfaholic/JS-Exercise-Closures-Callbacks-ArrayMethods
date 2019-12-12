f3c3687d665b4f187be1af8ae06aa49f8f972006
Wolfaholic <melissa.mackie@live.com>
2019-12-12 14:11:08 -0800

    saving progress

--- index.js
+++ index.js
@@ -181,8 +181,11 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let runArr = [];
+  runners.forEach((runners) =>
+      runArr.push( `${runners.last_name}, ${runners.first_name}` ));
+  return runArr;
 }
 
 /**
@@ -197,8 +200,10 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  return runners.map(function (runners) {
+    return runners.first_name.toUpperCase();
+  });
 }
 
 /**
@@ -214,8 +219,11 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+    return runners.filter(function(runners) {
+    return runners.shirt_size === tShirtSize;
+  },)
+
 }
 
 /**
@@ -228,8 +236,10 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  return runners.reduce(function(sum, numbers) {
+  return sum+=numbers.donation;
+}, 0);
 }
 
 /////////////// CLOSURES ///////////////
